9c5fae0fb3713622d11d0c5ee875a36b3f427285
Shiul93 <lfllamas93@gmail.com>
2016-12-02 09:34:27 +0100

    Created android studio project

---
+++ app/src/main/java/com/mytechia/robobo/framework/robobo_sensing/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mytechia.robobo.framework.robobo_sensing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
